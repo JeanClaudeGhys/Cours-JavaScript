78960526348c38e174b84a41c8d9e93c80ac182d
JeanClaudeGhys <machinesweb59@gmail.com>
2022-09-12 16:04:04 +0200

    Commit javascript

---
+++ index.js
@@ -0,0 +1,65 @@
+/*class Livre {
+    constructor(titre, auteur, pages) {
+        this.titre  = titre;
+        this.auteur = auteur;
+        this.pages = pages;
+    }
+}
+
+let monLivre = new Livre("La planète des singes", "Pirre Boulle", 274);
+
+ console.log(monLivre.titre);*/
+
+/*let developers = ["Alphonse", "Joseph", "Marcel", "Louis", 3];
+/*-----------------------------------------------------------------------*/
+
+/*console.log(developers[0]);
+let nbreDePersonnes = developers.length;
+console.log(nbreDePersonnes);
+
+developers.push("Jacques");
+console.log(developers);
+
+developers.unshift("Gérard");
+console.log(developers);
+
+developers.pop();
+console.log(developers);
+-----------------------------------------------------------------------*/
+
+/*let estConnecte = true;
+
+if(estConnecte) {
+    console.log("Connexion établie");
+}
+else {
+    console.log("Connexion échoué");
+}
+-----------------------------------------------------------------------*/
+/*let maVar = false;
+
+let monMessage = "";
+if(maVar) {
+    let monMessage = "Bienvenu dans votre profil";
+}
+else {
+    let monMessage = "Non connecté";
+}
+
+console.log(monMessage)
+
+/*-----------------------------------------------------------------------*/
+let scorePSG = 12;
+let scoreRM = 12;
+
+if(scorePSG > scoreRM) {
+    console.log("Vous êtes en train de rêver !!")
+}
+else if(scorePSG < scoreRM) {
+    console.log("Logique !!")
+}
+else {
+    console.log("Quoi match nul !!")
+}
+
+
